f66f2c019e663b2a5de9a4ac68a2a23606137aed
Amit-Saha98 <114154372+Amit-Saha98@users.noreply.github.com>
2023-02-25 19:52:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+let tableData = [];
+
+ function addRowButton() {
+  const table = document.getElementById("myTable");
+  const newRow = table.insertRow(-1);
+
+  const idCell = newRow.insertCell(0);
+  const nameCell = newRow.insertCell(1);
+  const rollCell = newRow.insertCell(2);
+  const subjectCell = newRow.insertCell(3);
+  const marksCell = newRow.insertCell(4);
+  const markedByCell = newRow.insertCell(5);
+  const saveCell = newRow.insertCell(6);
+
+  const id = tableData.length + 1;
+  const name = document.createElement("input");
+  name.type = "text";
+  nameCell.appendChild(name);
+  const roll = document.createElement("input");
+  roll.type = "text";
+  rollCell.appendChild(roll);
+  const subject = document.createElement("input");
+  subject.type = "text";
+  subjectCell.appendChild(subject);
+  const marks = document.createElement("input");
+  marks.type = "number";
+  marksCell.appendChild(marks);
+  const markedBy = document.createElement("input");
+  markedBy.type = "email";
+  markedByCell.appendChild(markedBy);
+
+  idCell.innerHTML = id;
+  idCell.setAttribute("data-id", id);
+
+
+  const rowData = {
+    id: id,
+    student_name: name.value,
+    student_roll: roll.value,
+    subject: subject.value,
+    marks: marks.value,
+    markedBy: markedBy.value
+  };
+
+  tableData.push(rowData);
+
+  const saveButton = document.createElement("button");
+  saveButton.innerHTML = "Save";
+  saveButton.addEventListener("click", function() {
+    const rowId = idCell.getAttribute("data-id");
+    const rowIndex = tableData.findIndex(row => row.id === Number(rowId));//for loop to iterate over row
+    tableData[rowIndex].student_name = name.value;
+    tableData[rowIndex].student_roll = roll.value;
+    tableData[rowIndex].subject = subject.value;
+    tableData[rowIndex].marks = marks.value;
+     if (markedBy.value.includes("@")) {
+      tableData[rowIndex].markedBy = markedBy.value;
+    } else {
+      alert("Please enter a valid email address");
+    }
+  });
+
+  saveCell.appendChild(saveButton);
+};
+
+
